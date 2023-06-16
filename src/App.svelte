a0f5200271f81d8ab5ed29c57f4ec589689f367a
falcodebeer <falcodebeer@gmail.com>
2023-06-16 23:46:55 +0200

    - fixed chosenAllergens bug - styled ProductDetail page - global gradient background

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-  import { Link, Router, Route } from "svelte-routing";
+  import { Router, Route } from "svelte-routing";
   import Home from "./routes/Home.svelte";
   import Overview from "./routes/Overview.svelte";
-
-  export let url = "/";
 </script>
 
 <Router>
     <Route path="/" component={Home} />
     <Route path="/overview" component={Overview} />
 </Router>
+
+<style>
+	:global(body) {
+		background: linear-gradient(to top, rgb(217, 47, 85), rgb(255, 178, 120));
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+	}
+</style>
