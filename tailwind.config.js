b2a9d235f3336d0e95d5629ffb814ff18b1dae46
ZouhairEM <zouhairelmariami@gmail.com>
2023-06-16 12:30:03 +0200

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{svelte,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',          
+        }
+      },
+      colors: {
+        green: '#479442',
+        beige: '#f6ead4',
+        peach: '#f1c488',
+        brown: '#bd7b36'
+      }
+    },
   },
   plugins: [],
 }
