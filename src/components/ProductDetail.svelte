c9569f60e741a55ce78f5218cf744e82a06549ca
ZouhairEM <zouhairelmariami@gmail.com>
2023-06-17 00:50:10 +0200

    overview styling

--- src/components/ProductDetail.svelte
+++ src/components/ProductDetail.svelte
@@ -2,33 +2,34 @@
   export let code;
   let product;
   let apiText;
-  let crossReferenced = '';
+  let crossReferenced = "";
   async function findCrossAllergens() {
-		apiText = apiText.map(text => {
-			const thing = text.split(':')[1]?.trim();
-			if (thing) {
-				return thing
-			} else {
-				return text
-			}
-		});
+    apiText = apiText.map((text) => {
+      const thing = text.split(":")[1]?.trim();
+      if (thing) {
+        return thing;
+      } else {
+        return text;
+      }
+    });
 
     // Convert both arrays to lowercase
-    let chosenAllergens = JSON.parse(localStorage.getItem('allergens'))
-        if(chosenAllergens) {
-            chosenAllergens = chosenAllergens.map(allergen => allergen.toLowerCase());
-            apiText = apiText.map(text => text.toLowerCase());
+    let chosenAllergens = JSON.parse(localStorage.getItem("allergens"));
+    if (chosenAllergens) {
+      chosenAllergens = chosenAllergens.map((allergen) => allergen.toLowerCase());
+      apiText = apiText.map((text) => text.toLowerCase());
 
-            // Find the common elements in the two arrays
-            const commonElements = chosenAllergens.filter(allergen => apiText.includes(allergen));
-            crossReferenced = commonElements;
-        }
+      // Find the common elements in the two arrays
+      const commonElements = chosenAllergens.filter((allergen) => apiText.includes(allergen));
+      crossReferenced = commonElements;
+      console.log({ crossReferenced });
+    }
   }
   async function fetchData() {
     try {
       const response = await fetch(`https://cors-anywhere.herokuapp.com/https://world.openfoodfacts.org/api/2/product/${code}`);
       const data = await response.json();
-			product = data.product
+      product = data.product;
       apiText = product.allergens_tags;
       findCrossAllergens();
     } catch (error) {
@@ -36,16 +37,17 @@
     }
   }
   fetchData();
-
 </script>
 
 {#if product}
-<div class="py-4 mx-3 flex flex-col">
+  <div class="py-4 mx-3 flex flex-col">
     <h1 class="bg-slate-200 rounded-xl p-1 text-2xl font-bold">Product</h1>
     <h1 class="text-green justify-center">{product.brands}</h1>
-    <img class="mt-3 w-40 justify-center" src={product.image_url} alt="Scanned Product">
-    <h3 class="mt-3"> Those things in { product.brands } can kill you </h3>
-    <h1 class="text-2xl py-3 font-bold">Your Allergens</h1>
-    <h6 class="bg-slate-200 rounded-xl p-1"> { crossReferenced } </h6>
-</div>
+    <img class="mt-3 w-40 justify-center" src={product.image_url} alt="Scanned Product" />
+    <h3 class="mt-3">Those things in {product.brands} can kill you</h3>
+    <h1 class="text-2xl py-3 font-bold">Profile</h1>
+    {#if crossReferenced.length !== 0}
+      <h6 class="bg-slate-200 rounded-xl p-1">{crossReferenced}</h6>
+    {/if}
+  </div>
 {/if}
