81be13143182aee39e2ef933bc6c90485ce19487
wesley_delpeut <wesley.delpeut@capgemini.com>
2023-06-16 23:35:49 +0200

    Make button to navigate to overview page

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,25 +1,31 @@
 <script>
-  import Scanner from "../components/Scanner.svelte";
-  import ProductDetail from "../components/ProductDetail.svelte";
-	let foundCode = '';
+	import Plus from "svelte-material-icons/Plus.svelte";
+	import Scanner from "../components/Scanner.svelte";
+	import ProductDetail from "../components/ProductDetail.svelte";
+	let foundCode = "";
 	function magic(event) {
-		foundCode = event.detail.code
+		foundCode = event.detail.code;
 	}
 </script>
 
 {#if !foundCode}
-<Scanner on:code={magic}/>
+	<Scanner on:code={magic} />
 {:else}
-<ProductDetail code={foundCode} />
+	<ProductDetail code={foundCode} />
 {/if}
 
-<div class="floating">
-	<a href="/productdetail">ProductDetail</a>
-	<a href="/overview">Overview</a>
+<div>
+	<a href="/overview">
+		<span class="floating p-5" style="background-color: #D92F55; border-radius: 50%">
+			<Plus size="2em"/>
+		</span>
+	</a>
 </div>
 
 <style>
 	.floating {
 		position: absolute;
+		bottom: 2em;
+		right: 2em;
 	}
 </style>
